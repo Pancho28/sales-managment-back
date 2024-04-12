d759174eca1ef97c88a2cf6e3e4e0035972d349c
Francisco Bravo <francisco.bravoleiva@externos-cl.cencosud.com>
2024-04-11 21:06:48 -0400

    + looger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger();
+  const port = process.env.PORT || 3000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(port, '0.0.0.0', () => {
+    logger.log(`Server is running in port ${port}.`);
+  });
 }
 bootstrap();
